e9c4aa0b539ea9b2c83e0023374b35fa4c092935
toomas <toimik007@hotmail.com>
2022-01-17 17:03:06 +0200

    lisatud vihjeid.

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ sisend = open("sisend.txt", "r")
 rida = 0
 teksti_ridu = -1
 while 1:
-    sisend_rida = sisend.readline()
+    sisend_rida = sisend.readline() #sisend_rida on list. List tähtedest / märkidest, mis on ühel real.
     if not sisend_rida or rida >= teksti_ridu:
         break
     if rida == 0:
         teksti_ridu = int(sisend_rida)
-    else:#siin on teksti read 1-1000 tükki
+    else:
+        print(sisend_rida)
+
+        reapikkus=len(sisend_rida)
+        #nüüd teame kui pikk on rida, saame for tsükli teha.
+
+        #teine võimalus on kasutada for taht in sisend_rida:
+        #sellisel juhul korratakse tsüklit kuni on tähti reas
+
+
+        #siin on teksti read 1-1000 tükki
         #rida võib pidada listiks ehk kui meil on reaks sisend_rida="Jõuluvanal puna nina", siis sisend_rida[0] on J täht jne
         #listi pikkuse saab nääiteks käsuga len rea_pikkus=len(sisend_rida)
         #TODO: tuleb rida realt ja täht tähelt kokku lugeda kõikide tähtede kogused.
         #TODO:for tsükkel 0-rea pikkus
         #todo:teha list, kus iga tähe järel on esinemis kordade number
-        print(sisend_rida)
+
     rida += 1
 
 sisend.close() #faili sulgemine, et ei jääks mällu ripakile.
