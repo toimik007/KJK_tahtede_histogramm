dae5abf9b0e74009738534ce42685e232deeb3f0
toomas <toimik007@hotmail.com>
2022-01-26 22:08:43 +0200

    eemaldatud tapitahed

--- naide2.py
+++ naide2.py
@@ -1,33 +1,49 @@
 sisend = open("sisend.txt", "r")
 rida = 0
 teksti_ridu = -1
-histogram_tahed = [] #tähtede nimekiri
-histogram_kodused = [] #tähtede kogused
-tahtede_loendur = 0 #mitu erinevat tähte on leitud?
-leidsin_tahe=0
+histogram_tahed = [] #tahtede nimekiri
+histogram_kodused = [] #tahtede kogused
+tahtede_loendur = 0 #mitu erinevat tahte on leitud?
+leidsin_tahe = 0
 
-sisend_rida = sisend.readline() #sisend_rida on list. List tähtedest / märkidest, mis on ühel real.
+sisend_rida = sisend.readline() #sisend_rida on list. List tahtedest / markidest, mis on uhel real.
 teksti_ridu = int(sisend_rida)
 
 for r in range(0, teksti_ridu):
-    sisend_rida = sisend.readline() #järgmine rida failist
-    reapikkus = len(sisend_rida) #tähtede kogus mida läbivaatame
-    for taht in range(0, reapikkus): #vaatame tähed ükshaavaal
-        leidsin_tahe=0
-        for asukoht in range(0, len(histogram_tahed)): #tsülkkel, kus vaatame läbi siiani leitud tähed ja trükima ainult siis, kui leiame uue.
-            if histogram_tahed[asukoht] == sisend_rida[taht]: # kui täht on leitud, siis loeme ta ära.
-                histogram_kodused[asukoht] += 1 # kui täht on juba histogrammis, siis loeme ta ära
-                leidsin_tahe=1 # täht leitud histogrammist
-        if leidsin_tahe==0: #kui tähte ei leidnud histogrammist, siis lisame ta sinna.
-             histogram_tahed.append(sisend_rida[taht])#histogrammi tähtedele üks täht juurde
-             histogram_kodused.append(1) #loendur ka üks juurde.
-sisend.close() #faili sulgemine, et ei jääks mällu ripakile.
+    sisend_rida = sisend.readline() #jargmine rida failist
+    reapikkus = len(sisend_rida) #tahtede kogus mida labivaatame
+    for taht in range(0, reapikkus): #vaatame tahed ukshaavaal
+        print(taht)
+        leidsin_tahe = 0
+        for asukoht in range(0, len(histogram_tahed)): #tsulkkel, kus vaatame labi siiani leitud tahed ja trukima ainult siis, kui leiame uue.
+            if histogram_tahed[asukoht] == sisend_rida[taht]: # kui taht on leitud, siis loeme ta ara.
+                histogram_kodused[asukoht] += 1 # kui taht on juba histogrammis, siis loeme ta ara
+                leidsin_tahe = 1 # taht leitud histogrammist
+        if leidsin_tahe == 0 and not sisend_rida[taht].isspace(): #kui tahte ei leidnud histogrammist, siis lisame ta sinna.
+             histogram_tahed.append(sisend_rida[taht])#histogrammi tahtedele uks taht juurde
+             histogram_kodused.append(1) #loendur ka uks juurde.
+sisend.close() #faili sulgemine, et ei jaaks mallu ripakile.
 
-valjund = open("val.txt", "w")#siia faili tuleb tulemus trükkida
+#print(*histogram_tahed,sep=', ')
 
-        #TODO:vaja kirjutada histogramm väljundfaili etteandtud kujul.
+valjund = open("val.txt", "w")#siia faili tuleb tulemus trukkida
+valjund.write("Tere maailm"+"Ilus ilm"+"\r")
 
-valjund.close()#sulgeme selle faili ka ja siis on kõik tehtud
+
+
+
+
+
+
+
+
+
+
+
+
+#TODO:vaja kirjutada histogramm valjundfaili etteandtud kujul.
+
+valjund.close()#sulgeme selle faili ka ja siis on koik tehtud
 
 
 
