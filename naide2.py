26a755c46c5d58b1d32bbcdadb1b87770d91889a
toomas <toimik007@hotmail.com>
2022-02-09 17:01:06 +0200

    parandused mõned

--- naide2.py
+++ naide2.py
@@ -1,8 +1,9 @@
 sisend = open("sisend.txt", "r")
-rida = 0
-teksti_ridu = -1
-histogram_tahed = [] #tahtede nimekiri
-histogram_kodused = [] #tahtede kogused
+print(sisend)
+rida = 0 # muutuja rida, int, pole kasutatud.
+teksti_ridu = -1 #int, failise loetud ridade arv
+histogram_tahed = [] #  tahtede nimekiri
+histogram_kodused = [] #  tahtede koguse
 tahtede_loendur = 0 #mitu erinevat tahte on leitud?
 leidsin_tahe = 0
 
@@ -11,9 +12,8 @@ teksti_ridu = int(sisend_rida)
 
 for r in range(0, teksti_ridu):
     sisend_rida = sisend.readline() #jargmine rida failist
-    reapikkus = len(sisend_rida) #tahtede kogus mida labivaatame
+    reapikkus = len(sisend_rida) #tahtede kogus mida labivaatame, Kas seda reapikkust vaja on?
     for taht in range(0, reapikkus): #vaatame tahed ukshaavaal
-        print(taht)
         leidsin_tahe = 0
         for asukoht in range(0, len(histogram_tahed)): #tsulkkel, kus vaatame labi siiani leitud tahed ja trukima ainult siis, kui leiame uue.
             if histogram_tahed[asukoht] == sisend_rida[taht]: # kui taht on leitud, siis loeme ta ara.
@@ -24,22 +24,16 @@ for r in range(0, teksti_ridu):
              histogram_kodused.append(1) #loendur ka uks juurde.
 sisend.close() #faili sulgemine, et ei jaaks mallu ripakile.
 
-#print(*histogram_tahed,sep=', ')
 
 valjund = open("val.txt", "w")#siia faili tuleb tulemus trukkida
+
 valjund.write("Tere maailm"+"Ilus ilm"+"\r")
 
 
-
-
-
-
-
-
-
-
-
-
+rida = 0
+for ch in histogram_tahed:#kuniks listis tähti
+    valjund.write(ch+" "+str(histogram_kodused[rida])+"\r") #kirjutame faili nimekirja, igale reale täht ja tema kogus.
+    rida += 1 #anname teada, et järmine rida.
 
 #TODO:vaja kirjutada histogramm valjundfaili etteandtud kujul.
 
@@ -47,3 +41,25 @@ valjund.close()#sulgeme selle faili ka ja siis on koik tehtud
 
 
 
+
+
+
+
+
+
+
+
+#muutujate nimekiri
+#sisend *tyyp on IOwrapper, mis sisaldab andmeid faili kohta "sisend.txt"
+#rida *int, väljundi kirjutamisel loeb ridu, et koguses saaksid õigele reale.
+#teksti_ridu *int, sisendfaili esimeselt realt loetud ridade arv.
+#histogram_tahed *list, kuhu lisatakse tähed, mis on omakorda str vormaadis
+#histogram_kodused *list, kuhu liidetakse tähe leidmisel kogused int vormaadis
+#tahtede_loendur *int tähti reas.
+#r *int teksti rea number failis
+#leidsin_tahe *int, kui on 1 siis leiti uus täht, kui 0, siis ei leitud. kontrollitakse real 22
+#sisend_rida * str, terve rida failist.
+#reapikkus * int rea pikkus failist.
+#asukoht * int, for tsüklis asukoht real. (pole vaja kasutada isegi.)
+#ch *str täht listist histogram tähed.
+#valjund * tyyp on IOwrapper, mis sisaldab andmeid faili kohta "val.txt"
